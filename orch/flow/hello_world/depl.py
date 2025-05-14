15b5b6ea729eeaad5ac2376f6c5c04b9c6c49621
Vugar Yagubov <vugar.yagubov@flix.com>
2025-05-14 14:12:39 +0200

    Switch two deployments

--- orch/flow/hello_world/depl.py
+++ orch/flow/hello_world/depl.py
@@ -1,51 +1,26 @@
 from time import sleep
-from typing import Any, ClassVar
+from typing import ClassVar
 
-from prefect import get_run_logger, task
+from prefect import flow, task
 
 from orch.core.base import BaseDepl
 
 
-@task
-def get_secret_from_aws(secret_name: str) -> str:
-    # imitate getting secret from AWS Secret Manager
-    sleep(2)
-    if secret_name:
-        return "something"
-    else:
-        raise ValueError("secret_name is missing")
-    
-    
-@task
-def get_data_from_snowflake(query: str, secret_name: str) -> list[dict[str, Any]]:
-    # imitate querying the Snowflake
-    sleep(2)
-    logger = get_run_logger()
-    if query and secret_name:
-        logger.info("Snowflake returned 3 rows")
-        return [
-            {"a":5, "b": 6},
-            {"a":7, "b": 8},
-            {"a":9, "b": 10},
-        ]
-
-@task
-def produce_to_kafka(data: list[dict[str, Any]]) -> None:
-    logger = get_run_logger()
-    sleep()
-    # imitate producing to kafka
-    logger.info(f"Produced {len(data)} rows to ")
+@task(log_prints=True)
+def say_hello(name: str) -> None:
+    if name == "Error":
+        raise ValueError("This is expected to fail.")
+    if name == "Sleep":
+        sleep(120)
+    print(f"Hello, {name}!")
 
 
 class HelloWorld(BaseDepl):
-    description: ClassVar[str] = "Simple Demo deployment"
+    description: ClassVar[str] = "Simple Hello World deployment"
 
-    def flow() -> None:  # type: ignore
-        secret_name = "secret_name"
-        query = "select 1 as a"
+    def flow(names: list[str]) -> None:  # type: ignore
+        parallel_tasks = say_hello.map(names)  # most common parallel task
 
-        secret = get_secret_from_aws(secret_name=secret_name)
+        [task.result() for task in parallel_tasks]  # catch the result
 
-        data = get_data_from_snowflake(query=query, secret=secret)
-         
-        produce_to_kafka(data)
+
