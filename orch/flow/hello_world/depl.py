1c9131eb64a9df149daa48efafc44f4f72d0f980
Vugar Yagubov <vugar.yagubov@flix.com>
2025-06-27 18:02:12 +0200

    test classmethod 2

--- orch/flow/hello_world/depl.py
+++ orch/flow/hello_world/depl.py
@@ -21,7 +21,7 @@ class HelloWorld(BaseDepl):
 
     @classmethod
     def flow(cls, names: list[str]) -> None:  # type: ignore
-        names = [f"{name}{cls.a}" for name in names]
+        names = [f"{name}_{cls.a}_{HelloWorld.a}" for name in names]
         parallel_tasks = say_hello.map(names)  # most common parallel task
 
         [task.result() for task in parallel_tasks]  # catch the result
